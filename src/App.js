b8d4a960c5c19211c3a1a5160ef568db85ed8256
ParkJuseung <judang0322@gmail.com>
2022-10-04 23:51:52 +0900

    -

--- src/App.js
+++ src/App.js
@@ -28,7 +28,11 @@ function App() {
       {/* 목록 */}
       <Container>
       <Row>
-        <Col xs>First, but unordered</Col>
+        <Col xs>
+          <img src="/ocean.jpg" width="80%"/>
+          <h1>바닷가</h1>
+          <p>맑고 투명한 바다</p>
+        </Col>
         <Col xs={{ order: 12 }}>Second, but last</Col>
         <Col xs={{ order: 1 }}>Third, but second</Col>
       </Row>
